f1f688f98148618190f87fc3efd2444de2357548
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2025-09-15 22:35:03 +0300

    Add ch_case function

--- str.py
+++ str.py
@@ -2,3 +2,13 @@ def pr_str(s):
     if not isinstance(s, str):
         return "Очікується string"
     return s
+
+def ch_case(s):
+    if not isinstance(s, str):
+        return "Очікується string"
+    if s.isupper():
+        return "Великі"
+    elif s.islower():
+        return "Малі"
+    else:
+        return "Змішані"
